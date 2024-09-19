2f419caa027bbeede3e763e6da52fe006516022d
hashim-baig <hashim.baig17@gmail.com>
2024-09-19 09:08:39 +0530

    fix/cross origin issue

--- server/app.js
+++ server/app.js
@@ -13,25 +13,38 @@ const AuthController = require('./controllers/authController')
 
 const app = express();
 
+const allowedOrigins = process.env.ALLOWED_ORIGINS.split(',');
+
 app.use(cors({
-    origin: process.env.CORS_ORIGIN,
+    origin: function (origin, callback) {
+        // Allow requests with no origin (e.g., mobile apps, curl requests)
+        if (!origin) return callback(null, true);
+
+        // Check if the request origin is in the allowedOrigins array
+        if (allowedOrigins.indexOf(origin) !== -1) {
+            callback(null, true); // Allow the request
+        } else {
+            callback(new Error('Not allowed by CORS')); // Deny the request
+        }
+    },
+
     credentials: true
 }));
 app.use(express.json());
 
-const sessionStore = new MySQLStore({createDatabaseTable: true}, db);
+const sessionStore = new MySQLStore({ createDatabaseTable: true }, db);
 
 app.use(session({
-	secret: process.env.SESSION_SECRET,
+    secret: process.env.SESSION_SECRET,
     key: 'user_sid',
-	store: sessionStore,
-	resave: false,
-	saveUninitialized: false,
+    store: sessionStore,
+    resave: false,
+    saveUninitialized: false,
     cookie: {
         maxAge: 1000 * 60 * 5,
-        httpOnly: true,         
-        secure: true,         
-        sameSite: 'none'   
+        httpOnly: true,
+        secure: true,
+        sameSite: 'none'
     }
 }));
 
